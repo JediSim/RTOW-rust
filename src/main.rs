fff8ade214d4fa4daecb16283b55e6999b1781a6
PICHENOT <simpiccc@gmail.com>
2024-06-11 21:07:29 +0200

    add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use image::{ImageBuffer, RgbImage, Rgb};  
+use image::{ImageBuffer, RgbImage, Rgb};
+use indicatif::ProgressBar;
+use std::{thread::sleep, time::Duration};
 
 const IMAGE_WIDTH:u32 = 256;
 const IMAGE_HEIGHT:u32 = 256;
@@ -6,15 +8,17 @@ const IMAGE_HEIGHT:u32 = 256;
 fn main() {
 
     let mut image: RgbImage = ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
+    let pb = ProgressBar::new(IMAGE_HEIGHT as u64);
 
     // u32 for next calculation
     for j in 0..IMAGE_HEIGHT {
+        pb.inc(1);
         for i in 0..IMAGE_WIDTH {
             // convert in u8 for r and g
             image.put_pixel(i,j, Rgb::<u8>([i.try_into().unwrap(), j.try_into().unwrap(), 0]));
         }
     }
+    pb.finish();
 
     image.save("output.png").unwrap();
-    println!("Hello, world!");
 }
